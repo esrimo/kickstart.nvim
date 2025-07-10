1ea24ae0d3769d2d485e0981af2966571ae2b8e6
Esteban Rincon <esteban.rincon.mora@gmail.com>
2025-07-10 13:17:59 -0500

    feat: add ts,js and java support. Also setup kulala to replace postman via http files

---
+++ lua/config/plugins/telescope.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    -- or                              , branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+      require('telescope').setup {}
+      vim.keymap.set('n', '<space>ls', require('telescope.builtin').find_files)
+    end,
+  },
+}
