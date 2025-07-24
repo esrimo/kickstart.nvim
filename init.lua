8507fca3406036aa29d98d4f8912865cad6adbb4
Esteban Rincon <esteban.rincon.mora@gmail.com>
2025-07-24 10:29:14 -0500

    install and setup scratch pad, i.e: floating notes

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+--
 --[[
 
 =====================================================================
@@ -472,6 +473,38 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+    end,
+  },
+
+  {
+    'athar-qadri/scratchpad.nvim',
+    event = 'VeryLazy',
+    dependencies = { 'nvim-lua/plenary.nvim' }, -- don't forget to add this one if you don't have it yet!
+    keys = {
+      {
+        '<Leader>note',
+        function()
+          local sp = require 'scratchpad'
+          sp.ui:new_scratchpad()
+        end,
+        desc = 'open scratchpad notes',
+
+        vim.keymap.set({ 'n', 'v' }, '<leader>pn', function()
+          local sp = require 'scratchpad'
+          sp.ui:sync()
+        end, { desc = 'Push selection / current line to scratch pad' }),
+      },
+    },
+    config = function()
+      require('scratchpad'):setup {
+        settings = {
+          sync_on_ui_close = true,
+          title = 'Esrimo scratch pad, i.e Notes',
+        },
+        default = {
+          root_patterns = { '.git', 'package.json', 'README.md', 'pyproject.toml', 'build.gradle' },
+        },
+      }
     end,
   },
 
