9c140ac1e2cbd90559c4cdceb8f09262ef2d9641
Esteban Rincon <esteban.rincon.mora@gmail.com>
2025-09-20 13:24:52 -0500

    improve blink.cmp suggestions select on enter instead of <C-y> which was annooying

--- init.lua
+++ init.lua
@@ -1166,6 +1166,7 @@ require('lazy').setup({
         preset = 'default',
         ['<C-CR>'] = { 'show' }, -- trigger suggestions
         ['<C-Space>'] = false,
+        ['<CR>'] = {'accept', 'accept_and_enter', 'select_accept_and_enter', 'select_accept_and_enter'},
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
